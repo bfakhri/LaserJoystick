6ef2e42475badfb0df67df475b8595167df2f28d
Bijan Fakhri <bfakhri@asu.edu>
2020-04-09 16:06:28 -0700

    Kinda working.... Need to fix drivers

--- sod_model.py
+++ sod_model.py
@@ -12,8 +12,8 @@ class MSFE_Model(tf.keras.Model):
     Input/Output: (bs, h, w, c) -> (bs, h, w, f) 
     bs: batch size, c: channels, f: num features
     '''
-    def __init__(self, num_features):
-        super(Model, self).__init__()
+    def __init__(self):
+        super(MSFE_Model, self).__init__()
 
         # Params
         num_filters = 8
@@ -40,9 +40,9 @@ class MSFE_Model(tf.keras.Model):
         self.recon = []
         self.recon.append(tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), padding='same', activation='relu'))
         self.recon.append(tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), padding='same', activation='relu'))
-        self.recon.append(tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), padding='same', activation='relu'))
+        self.recon.append(tf.keras.layers.Conv2D(filters=3, kernel_size=(3,3), padding='same', activation='relu'))
 
-    def call(x, training=False):
+    def call(self, x, training=False):
         ''' 
         Trains as an autoencoder but provides features during inference
         '''
@@ -66,15 +66,36 @@ class MSFE_Model(tf.keras.Model):
 
         features_large = h
 
+        features_all = tf.concat([features_small, features_medium, features_large], axis=-1)
         if(training):
-            features_all = tf.concat([features_small, features_medium, features_large])
             # Reconstruct the original image
             for layer in self.recon:
                 features_all = layer(features_all)
 
             return features_all
         else:
-            return features_small, features_medium, features_large
+            #return features_small, features_medium, features_large
+            return features_all
         
-#class SOD_Model:
+class SOD_Model:
+    ''' 
+    Object detector
+    '''
+    def __init__(self):
+        super(SOD_Model, self).__init__()
+
+        # Model Params
+        num_filters = 8
+        output_size = 10
+
+        # Load the Feature Extractor Model
+        self.fe_model = MSFE_Model()
+        MSFE_Model.load('msfe_model.extension')
+
+        # Simple object detection layers
+        self.od_lyrs = []
+        self.od_lyrs.append(tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), padding='same', activation='relu'))
+        self.od_lyrs.append(tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), padding='same', activation='relu'))
+        self.od_lyrs.append(tf.keras.layers.Conv2D(filters=num_filters, kernel_size=(3,3), padding='same', activation='relu'))
+
 
--- train_sod_model.py
+++ train_sod_model.py
@@ -3,15 +3,28 @@ import numpy as np
 import tensorflow_datasets as tfds
 from sod_model import MSFE_Model
 
-# Load dataset
-ds, info = tfds.load(name='celeb_a', split=['train'], with_info=True)
+# Load/configure dataset
+(ds_train, ds_test), info = tfds.load(name='celeb_a', split=['train', 'test'], with_info=True)
 print(info)
+def imgr(sample):
+    img = tf.cast(sample['image'], tf.float32)/255.0
+    return (img, img)
+    #return img
+ds_train = ds_train.map(imgr, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.cache()
+#ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
 
 # Load model
-model = MSFE_Model
+model = MSFE_Model()
+model.compile(optimizer='adam', loss='mse')
 
 # Train model
-model.fit(x=ds, y=ds, epochs=1)
+#ds_input = Input(tensor=
+#model.fit(x=ds['image'], y=ds['image'], epochs=1)
+model.fit(x=ds_train, epochs=1)
 
 # Save model
 model.save('./msfe_model.tfmodel')
