49008d5a719632bcfc57f7075bd2b546d6f5af62
Bijan Fakhri <bfakhri@asu.edu>
2020-04-13 08:58:18 -0700

    training but getting OOM error

--- train_sod_model.py
+++ train_sod_model.py
@@ -2,6 +2,10 @@ import tensorflow as tf
 import numpy as np
 import tensorflow_datasets as tfds
 from sod_model import MSFE_Model
+# Hack to get it to work with RTX cards
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 # Load/configure dataset
 (ds_train, ds_test), info = tfds.load(name='celeb_a', split=['train', 'test'], with_info=True)
@@ -22,9 +26,11 @@ model = MSFE_Model()
 model.compile(optimizer='adam', loss='mse')
 
 # Train model
-#ds_input = Input(tensor=
-#model.fit(x=ds['image'], y=ds['image'], epochs=1)
-model.fit(x=ds_train, epochs=1)
+#tbCallBack = tf.keras.callbacks.TensorBoard(log_dir='./logs/', update_freq='batch', histogram_freq=0, write_graph=True, write_images=True)
+tbCallBack = tf.keras.callbacks.TensorBoard(log_dir='./logs/', update_freq='batch', histogram_freq=0, write_graph=False, write_images=True)
+#model.fit(x=ds_train, epochs=10, steps_per_epoch=10, callbacks=[tbCallBack])
+#model.fit(x=ds_train, epochs=10, callbacks=[tbCallBack])
+model.fit(x=ds_train, epochs=10)
 
 # Save model
 model.save('./msfe_model.tfmodel')
