c08ee6cefa8fb2405e07190aaed5833357353235
Bijan Fakhri <bfakhri@asu.edu>
2020-04-14 11:48:02 -0700

    Working, but not with uniform image sizes

--- test_coco.py
+++ test_coco.py
@@ -15,18 +15,27 @@ assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 # Params
-batch_size = 16 
+batch_size = 1
 img_size = 640
 
 # Load/configure dataset
 (ds_train, ds_test), info = tfds.load(name='coco/2017', split=['train', 'test'], with_info=True)
+
+# Data preperation
 def imgr(sample):
+    # Extract image
     img = sample['image']
+    # BB are proportions (x_min_prop, y_min_prop, x_max_prop, y_max_prop)
     bb = sample['objects']['bbox']
-    img = tf.image.pad_to_bounding_box(img, 0, 0, img_size, img_size)
+    # Resize the image by padding with zeros
+    #img = tf.image.pad_to_bounding_box(img, 0, 0, img_size, img_size)
+    # Cast and map to (0,1)
     img = tf.cast(img, tf.float32)/255.0
+    # Draw BBs
     img_bb = tf.squeeze(tf.image.draw_bounding_boxes(tf.expand_dims(img, axis=0), tf.expand_dims(bb, axis=0), [(255,255,255,100)]))
     return (img, img_bb)
+
+# Data pipeline definition
 ds_train = ds_train.map(imgr, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 ds_train = ds_train.batch(batch_size)
 ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
