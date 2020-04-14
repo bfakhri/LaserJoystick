506c2cf7f22934db892420b89d21fb5adc2c3499
Bijan Fakhri <bfakhri@asu.edu>
2020-04-14 08:52:10 -0700

    Init

---
+++ train_sod_model.py
@@ -0,0 +1,43 @@
+#################################################
+# Trains the Simple Object Detector (SOD) Model #
+#################################################
+
+import tensorflow as tf
+import numpy as np
+import tensorflow_datasets as tfds
+from sod_model import MSFE_Model
+import sys
+import os
+# Hack to get it to work with RTX cards
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# Params
+batch_size = 16 
+img_size = 640
+
+# Load/configure dataset
+(ds_train, ds_test), info = tfds.load(name='coco/2017', split=['train', 'test'], with_info=True)
+def imgr(sample):
+    img = sample['image']
+    img = tf.image.pad_to_bounding_box(img, 0, 0, img_size, img_size)
+    img = tf.cast(img, tf.float32)/255.0
+    return (img, img)
+ds_train = ds_train.map(imgr, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.batch(batch_size)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
+
+# Load feature extractor model, if it doesn't exist throw an error
+model_path = './msfe_model/'
+if(os.path.exists(model_path)):
+    print('Loading Multi-Scale Feature Extractor')
+    model = tf.keras.models.load_model(model_path)
+else:
+    print('Failed to find feature extractor model: '+model_path+'. Exiting....')
+    print('Train the model by running \`python train_msfe_model.py\'')
+    sys.exit()
+
+# Instantiate and train object detection model
+
