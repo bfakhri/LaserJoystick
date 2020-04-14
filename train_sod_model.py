5fdde0c0ded1e582e1770b5bacbf6d2d50e6020c
Bijan Fakhri <bfakhri@asu.edu>
2020-04-14 09:22:19 -0700

    U

--- train_sod_model.py
+++ train_sod_model.py
@@ -8,6 +8,7 @@ import tensorflow_datasets as tfds
 from sod_model import MSFE_Model
 import sys
 import os
+import datetime
 # Hack to get it to work with RTX cards
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
@@ -21,23 +22,42 @@ img_size = 640
 (ds_train, ds_test), info = tfds.load(name='coco/2017', split=['train', 'test'], with_info=True)
 def imgr(sample):
     img = sample['image']
+    bb = sample['objects']['bbox']
     img = tf.image.pad_to_bounding_box(img, 0, 0, img_size, img_size)
     img = tf.cast(img, tf.float32)/255.0
-    return (img, img)
+    return (img, bb)
 ds_train = ds_train.map(imgr, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 ds_train = ds_train.batch(batch_size)
 ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 
+# Make log directories
+base_logdir = './logs/'
+logdir = base_logdir + datetime.datetime.now().strftime('%d-%H%M%S')
+writer = tf.summary.create_file_writer(logdir)
+
 
 # Load feature extractor model, if it doesn't exist throw an error
-model_path = './msfe_model/'
-if(os.path.exists(model_path)):
+msfe_path = './msfe_model/'
+if(os.path.exists(msfe_path)):
     print('Loading Multi-Scale Feature Extractor')
-    model = tf.keras.models.load_model(model_path)
+    msfe = tf.keras.models.load_model(msfe_path)
 else:
-    print('Failed to find feature extractor model: '+model_path+'. Exiting....')
+    print('Failed to find feature extractor model: '+msfe_path+'. Exiting....')
     print('Train the model by running \`python train_msfe_model.py\'')
     sys.exit()
 
 # Instantiate and train object detection model
+sod_model = SOD_Model()
 
+for step,data in enumerate(ds_train):
+    # Extract features
+    features = msfe(data[0])
+    with tf.GradientTape() as tape:
+        # Do the object detection 
+        detections = sod_model(features)
+        # Calculate loss
+        #loss = something...
+        # Perform gradient update
+    with writer.as_default():
+        tf.summary.image('train/image', data[0], step=global_steps)
+        tf.summary.scalar('step', step, step=global_steps)
