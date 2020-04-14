49037acd4abe307263950ce45b2b208c5792efde
Bijan Fakhri <bfakhri@asu.edu>
2020-04-13 20:11:25 -0700

    Header

--- test_msfe.py
+++ test_msfe.py
@@ -1,3 +1,7 @@
+############################################################
+# Loads the multiscale feature extraction model to test it #
+############################################################
+
 import tensorflow as tf
 import numpy as np
 import datetime
