8caaf74367a9d5335167bb0b6391fe5b7b6323eb
Bijan Fakhri <bfakhri@asu.edu>
2020-04-16 08:39:26 -0700

    I caved and did a distorting resize....

--- test_coco.py
+++ test_coco.py
@@ -20,6 +20,7 @@ img_size = 640
 
 # Load/configure dataset
 (ds_train, ds_test), info = tfds.load(name='coco/2017', split=['train', 'test'], with_info=True)
+print(info)
 
 # Data preperation
 def imgr(sample):
@@ -29,16 +30,18 @@ def imgr(sample):
     bb = sample['objects']['bbox']
     # Resize the image by padding with zeros
     #img = tf.image.pad_to_bounding_box(img, 0, 0, img_size, img_size)
+    img = tf.image.resize(img, (img_size, img_size))
     # Cast and map to (0,1)
     img = tf.cast(img, tf.float32)/255.0
     # Draw BBs
     img_bb = tf.squeeze(tf.image.draw_bounding_boxes(tf.expand_dims(img, axis=0), tf.expand_dims(bb, axis=0), [(255,255,255,100)]))
-    return (img, img_bb)
+    #return (img, img_size, img_bb)
+    return (img, img_bb, bb)
 
 # Data pipeline definition
 ds_train = ds_train.map(imgr, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 ds_train = ds_train.batch(batch_size)
-ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+#ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 
 # Make log directories
 base_logdir = './logs/'
@@ -48,9 +51,11 @@ writer = tf.summary.create_file_writer(logdir)
 
 for step,data in enumerate(ds_train):
     print('Step: ', step)
-    imgs = data[0]
-    bbs = data[1]
+    img = data[0]
+    bb_img = data[1]
+    bb = data[2]
+    print(bb)
     with writer.as_default():
-        tf.summary.image('train/image', imgs, step=step)
-        tf.summary.image('train/bb_img', bbs, step=step)
+        tf.summary.image('train/image', img, step=step)
+        tf.summary.image('train/bb_img', bb_img, step=step)
         tf.summary.scalar('step', step, step=step)
